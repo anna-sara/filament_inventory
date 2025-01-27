4de915aa421875f4777a58f56e9212d51cbd7760
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-01-27 21:41:46 +0100

    Fix with texts

--- app/Filament/Resources/ReserveditemResourceUser.php
+++ app/Filament/Resources/ReserveditemResourceUser.php
@@ -20,9 +20,7 @@ class ReserveditemResourceUser extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-archive-box-x-mark';
 
-    protected static ?string $modelLabel = 'Reserve an item';
-
-    protected static ?string $title = 'Reserve an item';
+    protected static ?string $modelLabel = 'Reserve items';
 
     protected static ?string $slug = 'user';
 
