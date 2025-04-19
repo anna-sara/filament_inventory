e2913cae8f10ee91b33aad31a6ae9d7ae7058b82
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-04-19 20:01:22 +0200

    Test domain route removed

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use Inertia\Inertia;
 use App\Filament\Pages\Reserve;
 
 
-Route::group(['domain' => 'boka.vbytes.se'], function(){
+//Route::group(['domain' => 'boka.vbytes.se'], function(){
     Route::get('/', Reserve::class);
-});
+//});
 
 
 require __DIR__.'/auth.php';
