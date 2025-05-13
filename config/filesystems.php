e7342f1696597165b82eb7bd76086baa0a2a268d
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-05-13 21:48:32 +0200

    Test cdn

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,6 +40,7 @@ return [
         'public' => [
             'driver' => 'local',
             //'root' => storage_path('app/public'),
+            'root' => '',
             //'url' => env('APP_URL').'/storage',
             'url' => 'media.boka.vbytes.se',
             'visibility' => 'public',
