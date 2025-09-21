b5844f0f48b13700a2c62517a897f10bfffee6b8
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-09-21 11:28:26 +0200

    Removed composer to repo

--- app/Filament/Widgets/AdminWidget.php
+++ app/Filament/Widgets/AdminWidget.php
@@ -19,8 +19,8 @@ class AdminWidget extends BaseWidget
             //Card::make(__('Total number of users'), User::count() ),
             Card::make(__('Total number of games'), Item::where('type', 'game')->count() ),
             Card::make(__('Total number of items'), Item::where('type', 'item')->count() ),
-            Card::make(__('Reservations at the moment'), Reserveditem::where('returned_date', null)->withTrashed()->count() ),
-            Card::make(__('Reservations over time'), Reserveditem::withTrashed()->count() ),
+            Card::make(__('Reservations at the moment'), Reserveditem::where('returned_date', null)->count() ),
+            Card::make(__('Reservations over time'), Reserveditem::withTrashed()->withTrashed()->count() ),
         ];
     }
 
