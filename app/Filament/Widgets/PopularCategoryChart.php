12c22f5069bda9a0e821493e06008150446b8d70
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-11-01 18:35:58 +0100

    Fixed widgets on admin view

--- app/Filament/Widgets/PopularCategoryChart.php
+++ app/Filament/Widgets/PopularCategoryChart.php
@@ -10,7 +10,7 @@ use App\Models\Item;
 
 class PopularCategoryChart extends ChartWidget
 {
-    protected static ?string $heading = 'Game reservations by category';
+    protected static ?string $heading = 'Reservations by category';
     protected static ?string $maxHeight = '275px';
 
     protected static ?array $options = [
@@ -26,18 +26,13 @@ class PopularCategoryChart extends ChartWidget
 
     protected function getData(): array
     {
-        $categories = Category::where('type', 'game')->get()->sortBy('id')->pluck('name');
+        $categories = Category::get()->sortBy('id')->pluck('name');
 
-        $items = Reserveditem::withTrashed()->with('item')->get()->groupBy('item.category_id')->sortBy('item.category_id', true);
+        $items = Reserveditem::withTrashed()->with('item')->get()->groupBy('item.category_id')->orderBy('item.category_id', 'desc');
         $itemCategoriesCount = [];
 
         foreach ($items as $item){
-            $item1 = Item::where('id', $item[0]->item_id)->get();
-            $type = Category::where('id', $item1[0]->category_id)->get();
-            if ($type[0]->type === "game") {
-                 $itemCategoriesCount[] = count($item);
-            }
-           
+            $itemCategoriesCount[] = count($item);
         }
 
         return [
